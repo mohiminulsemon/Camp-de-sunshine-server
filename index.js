75ce5afaf64516f454f3c694b1978731498f312c
mohiminulsemon <mohiminulsemon80@gmail.com>
2023-06-08 23:09:37 +0600

    completed basic setup and installation with mongo

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(cors(corsOptions))
 app.use(express.json())
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mq0mae1.mongodb.net/?retryWrites=true&w=majority`
+
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.02w34e6.mongodb.net/?retryWrites=true&w=majority"
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -26,9 +27,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    const usersCollection = client.db('aircncDb').collection('users')
-    const roomsCollection = client.db('aircncDb').collection('rooms')
-    const bookingsCollection = client.db('aircncDb').collection('bookings')
+    const usersCollection = client.db('campdb').collection('users')
+    // const roomsCollection = client.db('aircncDb').collection('rooms')
+    // const bookingsCollection = client.db('aircncDb').collection('bookings')
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 })
@@ -43,9 +44,9 @@ async function run() {
 run().catch(console.dir)
 
 app.get('/', (req, res) => {
-  res.send('AirCNC Server is running..')
+  res.send('camp Server is running..')
 })
 
 app.listen(port, () => {
-  console.log(`AirCNC is running on port ${port}`)
+  console.log(`camp is running on port ${port}`)
 })
